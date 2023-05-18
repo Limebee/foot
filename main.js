f695644f0044ba7d378881361c479f009cda44d0
LimeBee <kjeanlouis25@gmail.com>
2023-05-18 16:10:34 -0400

    add more fish info

--- main.js
+++ main.js
@@ -12,10 +12,12 @@ function getFetch(){
         
         document.querySelector('#species').innerText = data[0]['Species Name']
         document.querySelector('#scientific').innerText = data[0]['Scientific Name']
-        document.querySelector('ul').innerHTML = data[0].Habitat
-        document.querySelector('h4').innerHTML = data[0]['Physical Description']
+        document.querySelector('#habitat').innerHTML = data[0].Habitat
+        document.querySelector('#physicalDescription').innerHTML = data[0]['Physical Description']
         document.querySelector('#population').innerText = data[0].Population
         document.querySelector('#location').innerHTML = data[0].Location
+        document.querySelector('#fishingRate').innerHTML = data[0]['Fishing Rate']
+        document.querySelector('#populationStatus').innerHTML = data[0]['Population Status']
         document.querySelector('.fish').src = data[0]['Species Illustration Photo'].src
       })
       .catch(err => {
